0dd85e4d6383b4996f75fecd279abee6b2577114
Clément Cailly <ccailly@teclib.com>
2024-08-01 16:18:57 +0200

    feat(forms): Add ITILCategory field configuration for form destination

---
+++ phpunit/functional/Glpi/Form/Destination/CommonITILField/ITILCategoryFieldTest.php
@@ -0,0 +1,228 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace tests\units\Glpi\Form\Destination\CommonITILField;
+
+use DbTestCase;
+use Glpi\Form\AnswersHandler\AnswersHandler;
+use Glpi\Form\Destination\CommonITILField\ITILCategoryFieldConfig;
+use Glpi\Form\Destination\CommonITILField\ITILCategoryFieldStrategy;
+use Glpi\Form\Destination\FormDestinationTicket;
+use Glpi\Form\Form;
+use Glpi\Form\QuestionType\QuestionTypeITILCategory;
+use Glpi\Tests\FormBuilder;
+use Glpi\Tests\FormTesterTrait;
+use ITILCategory;
+use TicketTemplate;
+use TicketTemplatePredefinedField;
+
+final class ITILCategoryFieldTest extends DbTestCase
+{
+    use FormTesterTrait;
+
+    public function testSpecificITILCategory(): void
+    {
+        $form = $this->createAndGetFormWithMultipleITILCategoryQuestions();
+        $itilcategory = $this->createItem(ITILCategory::getType(), [
+            'name' => 'Test ITILCategory for specific value',
+        ]);
+
+        // Specific value
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: new ITILCategoryFieldConfig(
+                strategy: ITILCategoryFieldStrategy::SPECIFIC_VALUE,
+                specific_itilcategory_id: $itilcategory->getID()
+            ),
+            answers: [],
+            expected_itilcategory: $itilcategory->getID()
+        );
+
+        // No specific value
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: new ITILCategoryFieldConfig(
+                strategy: ITILCategoryFieldStrategy::SPECIFIC_VALUE,
+                specific_itilcategory_id: 0
+            ),
+            answers: [],
+            expected_itilcategory: 0
+        );
+    }
+
+    public function testITILCategoryFromSpecificQuestion(): void
+    {
+        $form = $this->createAndGetFormWithMultipleITILCategoryQuestions();
+        $itilcategories = $this->createItems(ITILCategory::getType(), [
+            ['name' => 'Test ITILCategory 1 for specific question'],
+            ['name' => 'Test ITILCategory 2 for specific question'],
+        ]);
+
+        // Using answer from first question
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: new ITILCategoryFieldConfig(
+                strategy: ITILCategoryFieldStrategy::SPECIFIC_ANSWER,
+                specific_question_id: $this->getQuestionId($form, "ITILCategory 1")
+            ),
+            answers: [
+                "ITILCategory 1" => $itilcategories[0]->getID(),
+                "ITILCategory 2" => $itilcategories[1]->getID(),
+            ],
+            expected_itilcategory: $itilcategories[0]->getID()
+        );
+
+        // Using answer from second question
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: new ITILCategoryFieldConfig(
+                strategy: ITILCategoryFieldStrategy::SPECIFIC_ANSWER,
+                specific_question_id: $this->getQuestionId($form, "ITILCategory 2")
+            ),
+            answers: [
+                "ITILCategory 1" => $itilcategories[0]->getID(),
+                "ITILCategory 2" => $itilcategories[1]->getID(),
+            ],
+            expected_itilcategory: $itilcategories[1]->getID()
+        );
+    }
+
+    public function testITILCategoryFromLastValidQuestion(): void
+    {
+        $form = $this->createAndGetFormWithMultipleITILCategoryQuestions();
+        $itilcategories = $this->createItems(ITILCategory::getType(), [
+            ['name' => 'Test ITILCategory 1 for last valid answer'],
+            ['name' => 'Test ITILCategory 2 for last valid answer'],
+            ['name' => 'Test ITILCategory 3 for last valid answer'],
+        ]);
+        $last_valid_answer_config = new ITILCategoryFieldConfig(
+            ITILCategoryFieldStrategy::LAST_VALID_ANSWER
+        );
+
+        // With multiple answers submitted
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: $last_valid_answer_config,
+            answers: [
+                "ITILCategory 1" => $itilcategories[0]->getID(),
+                "ITILCategory 2" => $itilcategories[1]->getID(),
+            ],
+            expected_itilcategory: $itilcategories[1]->getID()
+        );
+
+        // Only first answer was submitted
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: $last_valid_answer_config,
+            answers: [
+                "ITILCategory 1" => $itilcategories[0]->getID(),
+            ],
+            expected_itilcategory: $itilcategories[0]->getID()
+        );
+
+        // Only second answer was submitted
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: $last_valid_answer_config,
+            answers: [
+                "ITILCategory 2" => $itilcategories[1]->getID(),
+            ],
+            expected_itilcategory: $itilcategories[1]->getID()
+        );
+
+        // No answers, fallback to default value
+        $this->sendFormAndAssertTicketType(
+            form: $form,
+            config: $last_valid_answer_config,
+            answers: [],
+            expected_itilcategory: 0
+        );
+    }
+
+    private function sendFormAndAssertTicketType(
+        Form $form,
+        ITILCategoryFieldConfig $config,
+        array $answers,
+        int $expected_itilcategory,
+    ): void {
+        // Insert config
+        $destinations = $form->getDestinations();
+        $this->assertCount(1, $destinations);
+        $destination = current($destinations);
+        $this->updateItem(
+            $destination::getType(),
+            $destination->getId(),
+            ['config' => ['itilcategory' => $config->jsonSerialize()]],
+            ["config"],
+        );
+
+        // The provider use a simplified answer format to be more readable.
+        // Rewrite answers into expected format.
+        $formatted_answers = [];
+        foreach ($answers as $question => $answer) {
+            $key = $this->getQuestionId($form, $question);
+            // Real answer will be decoded as string by default
+            $formatted_answers[$key] = (string) $answer;
+        }
+
+        // Submit form
+        $answers_handler = AnswersHandler::getInstance();
+        $answers = $answers_handler->saveAnswers(
+            $form,
+            $formatted_answers,
+            getItemByTypeName(\User::class, TU_USER, true)
+        );
+
+        // Get created ticket
+        $created_items = $answers->getCreatedItems();
+        $this->assertCount(1, $created_items);
+        $ticket = current($created_items);
+
+        // Check ITILCategory
+        $this->assertEquals($expected_itilcategory, $ticket->fields['itilcategories_id']);
+    }
+
+    private function createAndGetFormWithMultipleITILCategoryQuestions(): Form
+    {
+        $builder = new FormBuilder();
+        $builder->addQuestion("ITILCategory 1", QuestionTypeITILCategory::class);
+        $builder->addQuestion("ITILCategory 2", QuestionTypeITILCategory::class);
+        $builder->addDestination(
+            FormDestinationTicket::class,
+            "My ticket",
+        );
+        return $this->createForm($builder);
+    }
+}
